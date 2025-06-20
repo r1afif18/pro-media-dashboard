3a50ac718dac611440bd9b1c55726b3c58ae6125
Rafif.s <165122312+r1afif18@users.noreply.github.com>
2025-06-20 18:02:34 +0700

    Delete components/__init__.py

--- components/__init__.py
+++
@@ -1,6 +0,0 @@
-from .tab_about import show as tab_about
-from .tab_ai_lab import show as tab_ai_lab
-from .tab_forecasting import show as tab_forecasting
-from .tab_insights import show as tab_insights
-from .tab_overview import show as tab_overview
-from .tab_upload import show as tab_upload
