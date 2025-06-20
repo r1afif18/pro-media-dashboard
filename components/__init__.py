f5278dccaa6ecb144c6f6a2738f0b43403a2c920
Rafif.s <165122312+r1afif18@users.noreply.github.com>
2025-06-20 17:04:16 +0700

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,1 +1,7 @@
+from .tab_overview import tab_overview
+from .tab_upload import tab_upload
+from .tab_ai_lab import tab_ai_lab
+from .tab_forecasting import tab_forecasting
+from .tab_insights import tab_insights
+from .tab_about import tab_about
 
