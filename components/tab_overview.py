ececfd3fa1f2b254c0e52824bf8974bd992f22dc
Rafif.s <165122312+r1afif18@users.noreply.github.com>
2025-06-20 15:32:45 +0700

    Update tab_overview.py

--- components/tab_overview.py
+++ components/tab_overview.py
@@ -3,4 +3,6 @@ import streamlit as st
 def tab_overview(tab):
     with tab:
         st.subheader("Overview")
-        st.write("Selamat datang di ProMedia Insight Hub! Dashboard ini menampilkan analisis media berbasis AI, eksplorasi data, insight, dan visualisasi berita.")
+        st.write(
+            "Selamat datang di ProMedia Insight Hub! Dashboard ini menampilkan analisis media berbasis AI, eksplorasi data, insight, dan visualisasi berita."
+        )
