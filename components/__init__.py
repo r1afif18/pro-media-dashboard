10babe77ddf4a9d62db3508f8816085073d80679
Rafif.s <165122312+r1afif18@users.noreply.github.com>
2025-06-20 17:23:00 +0700

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,1 +1,6 @@
-
+from .tab_about import show as tab_about
+from .tab_ai_lab import show as tab_ai_lab
+from .tab_forecasting import show as tab_forecasting
+from .tab_insights import show as tab_insights
+from .tab_overview import show as tab_overview
+from .tab_upload import show as tab_upload
